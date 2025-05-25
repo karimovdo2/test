0219224df44262959bbeadaad65b8a4e1164560f
karimovdo2 <karimovdo@yandex.ru>
2025-05-25 22:25:24 +0500

    fix: declare router and firebase deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import Poll from './Poll'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Survey from './Survey'
+import Doctor from './Doctor'
 
 export default function App() {
-  return <Poll />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/*" element={<Survey />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
